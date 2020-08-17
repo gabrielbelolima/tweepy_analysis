c537058c0071f1628b1021fe74541a3640517350
Gabriel Belo <43821561+gabrielbelolima@users.noreply.github.com>
2020-08-17 19:04:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from auth import auth
 from main_func import get_users_data, get_followers_data, get_seguidores_iguais, plot_tt_top
+
 tt = auth
 
-
-same_fol = get_seguidores_iguais('grabel','angie_marinho')
+same_fol = get_seguidores_iguais('profile_1','profile_2')
 print(same_fol)
 
-#gera plot dos seguidores em comum
 #plot_tt_top(same_fol)
-
-
