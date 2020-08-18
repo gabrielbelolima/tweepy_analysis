695d81c33afd06cd9ad5e42a72710b8e771cd2eb
Gabriel Belo <43821561+gabrielbelolima@users.noreply.github.com>
2020-08-18 18:11:37 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ def auth():
     '''Cria o objeto tt com a autenticação de DevTwitter''' 
     import tweepy
  
-    auth = ttweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(KEY, SECRET)
     global tt
     tt = tweepy.API(auth, wait_on_rate_limit=True)
